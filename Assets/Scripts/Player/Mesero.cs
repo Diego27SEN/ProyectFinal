187f821c892cdf650140fa8a6992ea91caa33a70
Diego27SEN <banitessebastian2decundaria@gmail.com>
2025-11-16 20:21:08 -0500

    Reapply "Merge branch 'main' of https://github.com/Diego27SEN/ProyectFinal"
    This reverts commit 6597992a1366b85fdda8bb9088bc27f1ef26456e.

--- Assets/Scripts/Player/Mesero.cs
+++ Assets/Scripts/Player/Mesero.cs
@@ -4,22 +4,56 @@ using UnityEngine.InputSystem;
 
 public class Mesero : MainMesero
 {
-
+    private float range = 1.5f;
+    private Vector2 dir;
+    private float degree = 90;
 
     void Update()
     {
         MovePlayer();
-        Cooldawn();
+        Cooldown();
         Contador();
+        Interaccion();
+        HandleMovement();
     }
 
-    public void Cooldawn()
+    public void Cooldown()
     {
 
         if (contador >= 40f && moveSpeed == 2)
         {
             moveSpeed++;
             contador = 0;
+        }
+    }
+    public void HandleMovement()
+    {
+        MovePlayer();
+
+        if (moveInput != Vector2.zero)
+            dir = moveInput.normalized;
+    }
+
+    public void Interaccion()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            print("Interaccionando");
+            Collider2D[] NPCS = Physics2D.OverlapCircleAll(transform.position, range);
+            foreach(Collider2D NPC in NPCS)
+            {
+                if(NPC.tag == "NPC")
+                {
+                    Vector2 npcDir = (NPC.transform.position - transform.position).normalized;
+                    float producto = Vector2.Dot(npcDir, dir);
+                    float toRadiasn = Mathf.Acos(producto);
+                    float toDegrees = toRadiasn * Mathf.Rad2Deg;
+                    if (toDegrees <= degree) 
+                    {
+                        print("Interaccion con " + "el NPC");
+                    }
+                }
+            }
         }
     }
 }
