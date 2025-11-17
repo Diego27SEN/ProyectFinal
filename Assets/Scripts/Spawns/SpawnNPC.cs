6597992a1366b85fdda8bb9088bc27f1ef26456e
Diego27SEN <banitessebastian2decundaria@gmail.com>
2025-11-16 20:21:01 -0500

    Revert "Merge branch 'main' of https://github.com/Diego27SEN/ProyectFinal"
    This reverts commit fbce84a7cbaebb4d4a50ac3618c8bed29bf63037, reversing
    changes made to ab43e25cbe88d2135ee6a3b023d2e1c9d4c138f7.

--- Assets/Scripts/Spawns/SpawnNPC.cs
+++ Assets/Scripts/Spawns/SpawnNPC.cs
@@ -5,16 +5,12 @@ public class SpawnNPC : MonoBehaviour
     [SerializeField] private GameObject NPCPrefab;
     [SerializeField] private float timeToSpawn;
     [SerializeField] private float currentTime;
-    [SerializeField] private Transform[] todasLasSillas;
+    [SerializeField] private Transform[] puntosB;
     private int indicePunto = 0;
 
     public void Update()
     {
         SpawnNPCTimer();
-    }
-     void Start()
-    {
-       
     }
     public void SpawnNPCTimer()
     {
@@ -31,10 +27,10 @@ public class SpawnNPC : MonoBehaviour
         GameObject npc = Instantiate(NPCPrefab, transform.position, Quaternion.identity); 
 
         NPC npcScript = npc.GetComponent<NPC>();
-        if (npcScript != null && todasLasSillas.Length > 0) // Verifica que el script NPC esté presente y que haya puntos B disponibles
+        if (npcScript != null && puntosB.Length > 0)
         {
-            npcScript.PUNTOB = todasLasSillas[indicePunto % todasLasSillas.Length]; // Asigna el punto B correspondiente
-            indicePunto++; // Incrementa el índice para el próximo NPC
+            npcScript.PUNTOB = puntosB[indicePunto % puntosB.Length]; 
+            indicePunto++;
         }
     }
 }
