70f31c9b62587eaee125bc5b7ee658062a1f6468
Diego27SEN <banitessebastian2decundaria@gmail.com>
2025-11-22 22:17:56 -0500

    Revert "Merge branch 'main' of https://github.com/Diego27SEN/ProyectFinal"
    This reverts commit 6d34a2f7fb6ea98b24a302c4e1cf42242aa3bccf, reversing
    changes made to 294a26eeccbfe0e12420ca4f8e35144d3cecf1aa.

--- Assets/Scripts/Interfaz/Reputation.cs
+++ Assets/Scripts/Interfaz/Reputation.cs
@@ -3,19 +3,5 @@ using UnityEngine;
 public class Reputation : MonoBehaviour
 {
     [SerializeField] private int reputation;
-    public int GetReputation()
-    {
-        return reputation;
-    }
 
-    public void AddReputation(int amount)
-    {
-        reputation += amount;
-    }
-
-    public void SubtractReputation(int amount)
-    {
-        reputation -= amount;
-        reputation = Mathf.Max(reputation, 0); // Evita valores negativos
-    }
 }
--- Assets/Scripts/Player/ControllerMesero.cs
+++ Assets/Scripts/Player/ControllerMesero.cs
@@ -17,8 +17,6 @@ public class ControllerMesero : MonoBehaviour
 
     [SerializeField] private string platoListoParaEntregar = "";
 
-    private Reputation reputationSystem;
-
     public float MoveSpeed
     {
         get => moveSpeed;
@@ -28,10 +26,6 @@ public class ControllerMesero : MonoBehaviour
     private void Awake()
     {
         inputs = new InputSystem_Actions();
-        // Busca el objeto GameManager y obtiene el componente Reputation
-        var gm = GameObject.Find("GameManager");
-        if (gm != null)
-            reputationSystem = gm.GetComponent<Reputation>();
     }
 
     private void OnEnable()
@@ -123,15 +117,10 @@ public class ControllerMesero : MonoBehaviour
                 if (npc != null && platoListoParaEntregar == npc.PedidoActual && !npc.PedidoEntregado)
                 {
                     npc.PedidoEntregado = true;
-                    npc.GetComponent<ControllerNPC>().RecibioComida = true;
+                    npc.GetComponent<ControllerNPC>().RecibioComida = true; // Activa la salida
                     Debug.Log("Pedido entregado al NPC: " + platoListoParaEntregar);
-
-                    // Actualiza la reputación global usando la reputación del NPC
-                    if (reputationSystem != null)
-                        reputationSystem.AddReputation(npc.Reputacion);
-
-                    platoListoParaEntregar = "";
-                    pedidoActualMesero = "";
+                    platoListoParaEntregar = ""; // Limpia el plato entregado
+                    pedidoActualMesero = "";     // Limpia el pedido pendiente del mesero
                 }
             }
         }
